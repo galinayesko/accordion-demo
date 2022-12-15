601725d8b50d618c1d8dbb370f070435d5ad40c3
Galina Yesko <gyesko@fmv.local>
2022-12-15 10:19:44 +0300

    accordion demo with tailwindcss

---
+++ js/script.js
@@ -0,0 +1,27 @@
+//JavaScript açma işlemi
+
+//  function ac () {
+//      var acik1 = document.getElementById("acik1").style.display="block";
+// }
+
+//  function ac2 () {
+//      var acik2 = document.getElementById("acik2").style.display="block";
+// }
+
+// function ac3 () {
+//     var acik3 = document.getElementById("acik3").style.display="block";
+// } 
+
+// Jquery Toggle
+
+function ac() {
+  $('#acik1').toggle(800);
+}
+
+function ac2() {
+  $('#acik2').toggle(800);
+}
+
+function ac3() {
+  $('#acik3').toggle(800);
+}
